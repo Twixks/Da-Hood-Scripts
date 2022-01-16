38285b1bb33de35f217a5478c54fe8ac3eabf903
xaxa <80309202+xaxaxaxaxaxaxaxaxa@users.noreply.github.com>
2022-01-16 04:10:59 -0600

    Update No-Jump-Cooldown.lua

--- No-Jump-Cooldown.lua
+++ No-Jump-Cooldown.lua
@@ -1,8 +1,17 @@
+-- init, for autoexec support
+if not game.IsLoaded(game) then 
+    game.Loaded.Wait(game.Loaded);
+end
+
+-- variables 
+local IsA = game.IsA
 local newindex = nil 
+
+-- main hook
 newindex = hookmetamethod(game, "__newindex", function(self, Index, Value)
-    if not checkcaller() and self.IsA(self, "Humanoid") and Index == "JumpPower" then 
+    if not checkcaller() and IsA(self, "Humanoid") and Index == "JumpPower" then 
         return
     end
     
-    return newindex(self, Index, Value)
+    return newindex(self, Index, Value);
 end)
